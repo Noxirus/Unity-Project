3e4c7b55a67f988b29df88aa107be5a3cf7d8ccc
Hamish Harrison <57675214+Noxirus@users.noreply.github.com>
2025-05-15 10:13:23 -0600

    Initial Input Controller Bindings
    Added Input Controller script to handle various game control input actions.
    Added Jump, Cancel Jump and Move Actions to the input controller.
    Will enable input upon monobehavior creation.

---
+++ Assets/Scripts/InputController.cs
@@ -0,0 +1,41 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputController : MonoBehaviour
+{
+    private GameControls _gameControls;
+
+    public event Action JumpEvent;
+    public event Action JumpEventCancelled;
+    public event Action<Vector2> MoveEvent;
+    
+    private void Awake()
+    {
+        _gameControls = new GameControls();
+    }
+
+    private void OnEnable()
+    {
+        _gameControls.Player.Enable();
+        
+        _gameControls.Player.Move.performed += OnMovePerformed;
+        _gameControls.Player.Jump.performed += OnJumpPerformed;
+        _gameControls.Player.Jump.canceled += OnJumpCanceled;
+    }
+
+    private void OnMovePerformed(InputAction.CallbackContext context)
+    {
+        MoveEvent?.Invoke(context.ReadValue<Vector2>());
+    }
+
+    private void OnJumpPerformed(InputAction.CallbackContext context)
+    {
+        JumpEvent?.Invoke();
+    }
+
+    private void OnJumpCanceled(InputAction.CallbackContext context)
+    {
+        JumpEventCancelled?.Invoke();
+    }
+}
